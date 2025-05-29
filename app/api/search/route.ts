1d688b98d751339006363cd85257cab8d0ad3624
qalbeali <qalbeali8@gmail.com>
2025-05-30 00:30:23 +0500

    blog added

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,4 +1,4 @@
-import { source } from '@/lib/source';
-import { createFromSource } from 'fumadocs-core/search/server';
+import { docsSource as source } from "@/lib/source";
+import { createFromSource } from "fumadocs-core/search/server";
 
 export const { GET } = createFromSource(source);
